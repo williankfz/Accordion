672a3b6a521f57c8ca7dfaec8c6b9bdfd2a1e3bd
Antonio Willian <antoniowillian075@gmail.com>
2023-05-22 21:04:57 -0300

    Accordion

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function initAccordion(){
+  const question = document.querySelectorAll('[data-faq="question"]');
+
+  if(question.length){
+    question[0].classList.add('active');
+    question[0].nextElementSibling.classList.add('active');
+  }
+
+  function accordion(){
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+  }
+
+  question.forEach((item) =>{
+    item.addEventListener('click',accordion);
+  });
+
+}
+
+initAccordion();
